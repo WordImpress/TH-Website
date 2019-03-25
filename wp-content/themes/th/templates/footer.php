749052a3007dff0395758e59f24f6e1ec6a611d3
Devin Walker <sdtucson@gmail.com>
2019-03-25 15:01:47 -0700

    chore: remove footer phone number and update address across site

--- wp-content/themes/th/templates/footer.php
+++ wp-content/themes/th/templates/footer.php
@@ -18,10 +18,9 @@
 
 			<a class="footer-brand" href="<?php echo home_url(); ?>/"><span><?php bloginfo( 'name' ); ?></span></a>
 
-			<address>433 G Street #201 <br>San Diego,CA 92101</address>
+			<address>433 G Street #204 <br>San Diego,CA 92101</address>
 
-			<p class="contact-info"><a href="mailto:contact@thoughthouse.com">contact@thoughthouse.com</a> <br />call
-				<a href="tel:16195505601">1(619)550-5601</a></p>
+			<p class="contact-info"><a href="mailto:contact@thoughthouse.com">contact@thoughthouse.com</a>
 
 		</div>
 
